2c6d399238d9081f14bd8cb484fc67cf95c0a4d8
darknx <152974900+drknx@users.noreply.github.com>
2025-01-23 19:56:00 +0530

    digit_recognition.py

--- digit_recognition.py
+++ digit_recognition.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageDraw
 import numpy as np
 import os
 
-# load and preprocess the dataset (digits 0-9) with binary pixel values
+# random shit starts
 def load_and_preprocess_data():
     (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
@@ -16,6 +16,7 @@ def load_and_preprocess_data():
     x_test = x_test.reshape(-1, 28, 28, 1)
 
     return (x_train, y_train), (x_test, y_test)
+# random shit ends
 
 def build_model():
     model = keras.Sequential([
